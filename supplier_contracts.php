70eca095290cc6008e43d28d21364a3101b38efa
Gerry Mae Joy Ramoso <ramosogerrymaejoy@gmail.com>
2026-09-01 00:24:50 +0800

    updated the connection of the svm submodules

--- supplier_contracts.php
+++ supplier_contracts.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
     exit();
 }
 
-require_once 'db_connection.php';
+require_once 'svm_connection.php';
 
 $section_title = "Supplier / Vendor Management";
 $admin_user = $_SESSION['username'] ?? 'Admin User';
