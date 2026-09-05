3daf038ebe9c708eb4e43791c664a81017ee7dc2
Fran Jy <franvaliente24@gmail.com>
2026-09-05 18:24:15 +0800

    Update database server name in sws_connection.php

--- sws_connection.php
+++ sws_connection.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 error_reporting(0);
 
-$servername = getenv('DB_SWS_HOST')     ?: getenv('DB_HOST')     ?: 'maria-n9o7nsa7.internal';
+$servername = getenv('DB_SWS_HOST')     ?: getenv('DB_HOST')     ?: 'mariadb-n9o7nsa7.internal';
 $username   = getenv('DB_SWS_USERNAME') ?: getenv('DB_USERNAME') ?: 'hf_ejcnux6zyo';
 $password   = getenv('DB_SWS_PASSWORD') ?: getenv('DB_PASSWORD') ?: 'bg0uMqT7KfIBEcyKRgfg1qimuT6cy9Bv';
 $dbname     = getenv('DB_SWS_DATABASE') ?: getenv('DB_DATABASE') ?: 'hf_db_n9o7nsa7';
